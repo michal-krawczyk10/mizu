a3520dc08fba973b8a18ce298530c0590b2461d5
Michał Krawczyk <michal.krawczyk10@gmail.com>
2020-11-13 10:21:26 +0100

    counter in progress

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,25 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
+import { registerSW } from "./pwa.js";
 registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
+
+const addButton = document.querySelector(".button__add--js");
+const removeButton = document.querySelector(".button__remove--js");
+
+const score = 0;
+
+function createScore(querySelectorContent, content) {
+	const score = document.querySelector(querySelectorContent);
+	score.innerHTML = content;
+}
+
+createScore(".counter--js", 0);
+
+const add = score++;
+const remove = score--;
 
