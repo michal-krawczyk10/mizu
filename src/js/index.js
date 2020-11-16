477ee673a6ce84d719579d38321710a69f0bbf88
Michał Krawczyk <michal.krawczyk10@gmail.com>
2020-11-16 21:53:37 +0100

    remove event blocked for value <= 0

--- src/js/index.js
+++ src/js/index.js
@@ -12,8 +12,7 @@ const addButton = document.querySelector(".button__add--js");
 const removeButton = document.querySelector(".button__remove--js");
 
 let score = 0;
-
-const myResult = score;
+let myResult = score;
 
 function createScore(querySelectorContent, content) {
 	let myResult = document.querySelector(querySelectorContent);
@@ -28,12 +27,14 @@ function add() {
 	myResult.innerHTML = score;
 }
 
-addButton.addEventListener('click', add);
+addButton.addEventListener("click", add);
 
 function remove() {
 	let myResult = document.querySelector(".counter--js");
-  score--;
-  myResult.innerHTML = score;
+	if (score > 0) {
+		score--;
+	}
+	myResult.innerHTML = score;
 }
 
-removeButton.addEventListener('click', remove);
+removeButton.addEventListener("click", remove);
